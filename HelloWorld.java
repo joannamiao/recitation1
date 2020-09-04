2bb0a3a8f40a1817d7da1e533978991a3bc6e359
Joanna Miao <joannam1@andrew.cmu.edu>
2020-09-04 11:13:52 -0700

    added how many years I've been here to HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World. I've been here for 3 years");
+    }
+
+}
